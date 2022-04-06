5fb75075d39593602013b9f9e59f3b4198313f35
morariudenis <morariudenis@gmail.com>
2022-04-06 15:08:02 +0300

    First commit

---
+++ Introduction to Python/Teme/Tema_11.py
@@ -0,0 +1,40 @@
+# Avand ca exemplu pagina https://the-internet.herokuapp.com/login
+#
+# Feature: User Authentication
+#
+# Mergeti backwards si ganditi ca un product owner
+#
+# Generati un epic (doar o propozitie care sa descrie functionalitatea majora ce urmeaza sa fie dezvoltata)
+#
+# Generati un user story pentru login care sa aiba 2 acceptance criteria
+# Un login valid (positive)
+# Un login Invalid (negative)
+#
+# Generati un alt user story care acopere functionalitatea de register. Sa contina 2 acceptance criteria
+# Email e nou, deci user poate face register (positive)
+# Email e deja existent in baza de date (negative)
+#
+# Generati un ultim user story care sa acopere functionalitatea de reset password. Sa contina 2 acceptance criteria
+# Email e in baza de date
+# Email nu e in baza de date
+#
+# Reminder:
+# Un user story are forma
+# As a …
+# I want to …
+# So that …
+#
+# Un Acceptance criteria are forma
+# Given …
+# When …
+# Then …
+#
+# Impementati 3 pagini in noul proiect BDD cu POM
+# Home page https://the-internet.herokuapp.com/
+# Sa aiba cel putin 3 elemente inlcusiv Form Authentication
+# Sa contina metode de click pe ele
+# Login page https://the-internet.herokuapp.com/login
+# Sa contina user, pass, login_btn si metode pt interactiune cu ele
+# Secure page https://the-internet.herokuapp.com/secure
+# Sa contina mesajul de succes si verificare ca e displayed
+# Sa contina logout_btn si click pe el
